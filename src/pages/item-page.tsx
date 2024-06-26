4c620ebcabbf24a95d7e6300798393b60bb1c107
bogdan shulika <bsshul@gmail.com>
2024-06-26 12:46:50 +0200

    Folders

--- src/pages/item-page.tsx
+++ src/pages/item-page.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 export default function ItemPage() {
   const { id } = useParams();
   const [localItems, setLocalItems] = useLocalStorage<ItemProps[]>("items", []);
+  const [folderList, setFolderList] = useState(false);
 
   const item = localItems.filter((el) => el.id === id)[0];
 
@@ -49,6 +50,20 @@ export default function ItemPage() {
     }
   };
 
+  const uniqueFolders = [
+    ...new Set(localItems.map((item) => item.folder as string)),
+  ].filter((item) => item as string);
+
+  const filteredFolders =
+    folder === ""
+      ? uniqueFolders
+      : uniqueFolders.filter((el) =>
+          el
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(folder!.toLowerCase().replace(/\s+/g, ""))
+        );
+
   return (
     <>
       <section className="px-4">
@@ -75,14 +90,37 @@ export default function ItemPage() {
             className="mt-4 p-3 rounded-lg bg-white shadow-lg outline-none w-full bg-transparent scroll_textarea resize-none"
             placeholder="Enter text..."
           />
-          <input
-            type="text"
-            value={folder}
-            spellCheck="false"
-            onChange={(e) => setFolder(e.target.value)}
-            className="mt-4 outline-none w-full bg-transparent"
-            placeholder="Enter folder..."
-          />
+          <div className="relative">
+            <input
+              type="text"
+              value={folder}
+              spellCheck="false"
+              onChange={(e) => setFolder(e.target.value)}
+              className="mt-4 outline-none w-full bg-transparent"
+              placeholder="Enter folder..."
+              onClick={() => setFolderList(true)}
+            />
+
+            <div className="absolute left-0 -bottom-2 translate-y-full bg-white shadow-lg max-w-[200px] w-full rounded-md">
+              {folderList && (
+                <>
+                  {filteredFolders.map((folder) => (
+                    <button
+                      type="button"
+                      key={folder}
+                      onClick={() => {
+                        setFolder(folder);
+                        setFolderList(false);
+                      }}
+                      className="flex w-full py-1 px-2"
+                    >
+                      {folder}
+                    </button>
+                  ))}
+                </>
+              )}
+            </div>
+          </div>
           <div className="mt-4 flex items-center gap-3">
             <input
               id="pin"
